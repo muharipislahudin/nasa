0536919815e91f111c24b5d6c476dc2acc4867c4
muharipislahudin <43898480+muharipislahudin@users.noreply.github.com>
2018-10-06 10:24:13 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+         var url = "https://api.nasa.gov/planetary/apod?api_key=VYnZfFI4xoiszDPVRHGuUP2cDydx59nl7MfWCIvj";
         
         $("#loadImage").click(function(){
             $.ajax({
